34fa6a6042d1bcdb038fc30c79ade93f6adea701
Khoerodin <khoerodin@live.com>
2017-03-30 15:59:33 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+.checkout
+=========
+
+A Symfony project created on March 30, 2017, 9:32 am.
---
+++ src/AppBundle/Controller/TodoController.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: khoerodin
+ * Date: 3/30/17
+ * Time: 10:32 AM
+ */
+namespace AppBundle\Controller;
+
+
+use AppBundle\Entity\Todo;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * @Route("/todos", name="todo_index")
+     */
+    public function indexAction()
+    {
+        $todos = $this->getDoctrine()
+            ->getRepository('AppBundle:Todo')
+            ->findAll();
+
+        return $this->render('todo/index.html.twig', array(
+            'todos' => $todos
+        ));
+
+    }
+
+    /**
+     * @Route("/todo/create", name="todo_create")
+     */
+    public function createAction(Request $request)
+    {
+        $todo = new Todo();
+
+        $form = $this->createFormBuilder($todo)
+            ->add('name', TextType::class)
+            ->add('category', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('priority', ChoiceType::class, [
+                'choices' => [
+                    'Low' => 'low',
+                    'Medium' => 'medium',
+                    'High' => 'high']
+            ])
+            ->add('due_date', DateTimeType::class)
+            ->add('save', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $name = $form['name']->getData();
+            $category = $form['category']->getData();
+            $description = $form['description']->getData();
+            $priority = $form['priority']->getData();
+            $due_date = $form['due_date']->getData();
+
+            $now = new\DateTime('now');
+
+            $todo->setName($name);
+            $todo->setCategory($category);
+            $todo->setDescription($description);
+            $todo->setPriority($priority);
+            $todo->setDueDate($due_date);
+            $todo->setCreateDate($now);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($todo);
+            $em->flush();
+
+            $this->addFlash(
+                'notice',
+                'Todo Added'
+            );
+
+            return $this->redirectToRoute('todo_index');
+        }
+
+        return $this->render('todo/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+
+    }
+
+    /**
+     * @Route("/todo/edit/{id}", name="todo_edit")
+     */
+    public function editAction(Request $request, $id)
+    {
+        return $this->render('todo/edit.html.twig');
+
+    }
+
+    /**
+     * @Route("/todo/details/{id}", name="todo_details")
+     */
+    public function detailsAction($id)
+    {
+        $todo = $this->getDoctrine()
+            ->getRepository('AppBundle:Todo')
+            ->find($id);
+
+        return $this->render('todo/details.html.twig', array(
+            'todo' => $todo
+        ));
+
+    }
+
+}
